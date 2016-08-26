ec1d73d5e63f1d99e5376d6ddbad1256708e344e
taylorjames9 <taylorjames9@gmail.com>
2016-08-26 12:30:12 -0700

    alpha slider, new bats and all fixes to alpha blocks

---
+++ Assets/Scripts/WhiteScrim_Alpha.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WhiteScrim_Alpha : MonoBehaviour {
+
+	public Slider alpha_slider;
+
+	// Use this for initialization
+	void Start () {
+		alpha_slider.value = 0.33f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ChangeAlphaOfWhiteScrim(){
+
+		//int myFloat = System.Int16.Parse (textField.text);
+		//float.TryParse(textField.text, out myOut)
+
+		///float myOut = 0.33f;
+
+		GetComponent<Image> ().color = new Color (255f, 255f, 255f, alpha_slider.value);
+	}
+
+}
